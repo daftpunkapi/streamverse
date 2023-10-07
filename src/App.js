9bd4a5cc43419ca1f72125c4f17642a68768014d
raghavm23 <raghav23mehta@gmail.com>
2023-10-07 17:08:00 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Landscape from "./Landscape";
 import Toys from "./Toys";
 // import Table from "./Table";
 import * as React from "react";
+import { useState } from "react";
 import {
   Tabs,
   TabList,
@@ -14,20 +15,32 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabsChange = (index) => {
+    setTabIndex(index);
+  };
   return (
     <ChakraProvider>
       <div className="app-container" style={{ textAlign: "center" }}>
         <div className="title-container"> Streaming Stack</div>
-        <Tabs position="relative" variant="unstyled">
+        <Tabs
+          index={tabIndex}
+          onChange={handleTabsChange}
+          position="relative"
+          variant="unstyled"
+        >
           <TabList justifyContent="center" style={{ margin: "10px 0" }}>
             <Tab
               _selected={{ color: "black" }}
+              _hover={tabIndex !== 0 ? { backgroundColor: "lightgrey" } : {}}
               style={{ padding: "30px 30px" }}
             >
               Landscape
             </Tab>
             <Tab
               _selected={{ color: "black" }}
+              _hover={tabIndex !== 1 ? { backgroundColor: "lightgrey" } : {}}
               style={{ padding: "30px 30px" }}
             >
               Toys
