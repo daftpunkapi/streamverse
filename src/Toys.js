36fdaa0bb2a026bbec861e81c5e54685c5eafbb7
raghavm23 <raghav23mehta@gmail.com>
2023-10-07 12:52:42 +0530

    design edit

--- src/Toys.js
+++ src/Toys.js
@@ -9,16 +9,40 @@ function Toys() {
   return (
     <div className="imgcontainer">
       <div className="imgcard">
-        <img src={at} alt="Asset Tracking" />
+        <div className="imgcard-top">
+          <p>Description for Asset Tracking</p>
+          <button>Button for Asset Tracking</button>
+        </div>
+        <div className="imgcard-bottom">
+          <img src={at} alt="Asset Tracking" />
+        </div>
       </div>
       <div className="imgcard">
-        <img src={sif} alt="Sign-in Fraud" />
+        <div className="imgcard-top">
+          <p>Description for Sign-in Fraud</p>
+          <button>Button for Sign-in Fraud</button>
+        </div>
+        <div className="imgcard-bottom">
+          <img src={sif} alt="Sign-in Fraud" />
+        </div>
       </div>
       <div className="imgcard">
-        <img src={zom} alt="Restaurant Hyperpersonalization" />
+        <div className="imgcard-top">
+          <p>Description for Restaurant Hyperpersonalization</p>
+          <button>Button for Restaurant Hyperpersonalization</button>
+        </div>
+        <div className="imgcard-bottom">
+          <img src={zom} alt="Restaurant Hyperpersonalization" />
+        </div>
       </div>
       <div className="imgcard">
-        <img src={fx} alt="RT Stock Prices" />
+        <div className="imgcard-top">
+          <p>Description for RT Stock Prices</p>
+          <button>Button for RT Stock Prices</button>
+        </div>
+        <div className="imgcard-bottom">
+          <img src={fx} alt="RT Stock Prices" />
+        </div>
       </div>
     </div>
   );
