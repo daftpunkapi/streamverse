6689220b72dabc9b860f7463387d39b81dc632cd
raghavm23 <raghav23mehta@gmail.com>
2023-10-07 12:28:08 +0530

    Toys added

--- src/Table.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Toys() {
-  return <div>This is a table!</div>;
-}
-
-export default Toys;
--- src/Toys.js
+++ src/Toys.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Toys.css";
+import fx from "./PipelineImg/fx.png";
+import sif from "./PipelineImg/sif.png";
+import zom from "./PipelineImg/zom.png";
+import at from "./PipelineImg/asset.png";
 
 function Toys() {
-  return <div>HELLO TOYS!</div>;
+  return (
+    <div className="imgcontainer">
+      <div className="imgcard">
+        <img src={at} alt="Asset Tracking" />
+      </div>
+      <div className="imgcard">
+        <img src={sif} alt="Sign-in Fraud" />
+      </div>
+      <div className="imgcard">
+        <img src={zom} alt="Restaurant Hyperpersonalization" />
+      </div>
+      <div className="imgcard">
+        <img src={fx} alt="RT Stock Prices" />
+      </div>
+    </div>
+  );
 }
 
 export default Toys;
