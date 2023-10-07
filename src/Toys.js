66165c9ac2a691606c98ee9b950a89d8529aa06c
raghavm23 <raghav23mehta@gmail.com>
2023-10-07 17:38:37 +0530

    toys updated

--- src/Toys.js
+++ src/Toys.js
@@ -4,25 +4,25 @@ import fx from "./PipelineImg/fx.png";
 import sif from "./PipelineImg/sif.png";
 import zom from "./PipelineImg/zom.png";
 import at from "./PipelineImg/asset.png";
-import gh from "./gh.webp";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 function Toys() {
   return (
     <div className="imgcontainer">
       <div className="imgcard">
         <div className="imgcard-top">
-          <div class="content-container">
+          <p>
             Last Mile real-time IoT Asset Tracking i.e. driver data visibility &
             ETA to destination
-            <a
-              href="https://example.com"
-              target="_blank"
-              class="button-link"
-              rel="noreferrer"
-            >
-              <img src={gh} alt="repo link" class="button-image" />
-            </a>
-          </div>
+          </p>
+          <button
+            className="button"
+            onClick={() =>
+              window.open("https://github.com/daftpunkapi/asset-tracking-flink")
+            }
+          >
+            <u>GitHub</u> <ExternalLinkIcon />
+          </button>
         </div>
         <div className="imgcard-bottom">
           <img src={at} alt="Asset Tracking" />
@@ -34,7 +34,14 @@ function Toys() {
             Real-time Sign-In Fraud Detection based on Network & Digital Body
             Language of User
           </p>
-          <button>Button</button>
+          <button
+            className="button"
+            onClick={() =>
+              window.open("https://github.com/daftpunkapi/prototype-v2-sif")
+            }
+          >
+            <u>GitHub</u> <ExternalLinkIcon />
+          </button>
         </div>
         <div className="imgcard-bottom">
           <img src={sif} alt="Sign-in Fraud" />
@@ -46,7 +53,14 @@ function Toys() {
             Recsys pipeline to recommend Restaurants based on live location,
             order traffic & order history
           </p>
-          <button>Button</button>
+          <button
+            className="button"
+            onClick={() =>
+              window.open("https://github.com/daftpunkapi/prototype-v0.0.1")
+            }
+          >
+            <u>GitHub</u> <ExternalLinkIcon />
+          </button>
         </div>
         <div className="imgcard-bottom">
           <img src={zom} alt="Restaurant Hyperpersonalization" />
@@ -58,7 +72,12 @@ function Toys() {
             Converts live Stock Price of any Company from USD to INR using
             real-time FX rates
           </p>
-          <button>Button</button>
+          <button
+            className="button"
+            onClick={() => window.open("https://github.com/daftpunkapi/flinky")}
+          >
+            <u>GitHub</u> <ExternalLinkIcon />
+          </button>
         </div>
         <div className="imgcard-bottom">
           <img src={fx} alt="RT Stock Prices" />
